5adfe334d52207949cfedd50a0a8c919a87a1c86
YYC <1208709953@qq.com>
2019-10-21 23:14:50 +0800

    更新2

--- src/prettycat/lovelycat.java
+++ src/prettycat/lovelycat.java
@@ -3,7 +3,7 @@ package prettycat;
 public class lovelycat {
 	public static void main(String[] args) {
 		System.out.println("小花猫");
-	//	System.out.println("喵喵叫");
+		System.out.println("喵喵叫");
 	}
 
 }
